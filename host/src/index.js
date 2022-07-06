5907359400f9efd46875df816866ada24624b322
Sam <sam@greenhillfamily.co.uk>
2022-07-06 09:32:06 +0000

    Initial commit

---
+++ host/src/index.js
@@ -0,0 +1,4 @@
+// host/src/index.js
+import("./bootstrap");
+
+// Note: It is important to import bootstrap dynamically using import() otherwise you will also see the same error.
---
+++ remote/src/Button.js
@@ -0,0 +1,6 @@
+// remote/src/Button.js
+import React from "react";
+
+export const Button = () => <button>Hello!</button>;
+
+export default Button;
