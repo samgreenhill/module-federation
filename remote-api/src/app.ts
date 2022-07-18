174b77a7b78603a5bb15355b27e469d1f49cb10a
Sam <sam@greenhillfamily.co.uk>
2022-07-18 10:20:05 +0000

    Added cors headers to remote's API

--- remote-api/src/app.ts
+++ remote-api/src/app.ts
@@ -8,6 +8,11 @@ export const handler = async (
   console.log(`Event: ${JSON.stringify(event)}`);
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+    },
     body: "Hello World"
   }
 }
